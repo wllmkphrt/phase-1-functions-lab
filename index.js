693d7c63949bb29d2efedc996e74d06f4280a30a
William Kay Kephart <wllmkphrt@gmail.com>
2022-09-29 17:28:18 -0400

    commit tree 67a8e56d67e993677d4e3ad6c2fa23f4f94bfcd5

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    return Math.abs(location - 42);
+}
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000){
+        return Math.round((distanceTravelledInFeet(start, destination) * .02 - 400 * .02) * 100) / 100;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500){
+        return 25;
+    }
+    else return "cannot travel that far";
+}
